2dfacaa0bb6f9699c42db2a03598170243978725
Juan Zerbini Berro <juanze82@hotmail.com>
2017-09-19 18:54:43 +0200

    running on mongoose

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # mongo-basics
 node und mongoDB basic connection
+
+This is a really simple "REST API" app to explore how to create the following items:
+[an Endpoint] (https://en.wikipedia.org/wiki/Service-oriented_architecture#Web_2.0).
+Covering:
+| Verbs         | Operation     |
+| ------------- |:-------------:|
+| GET           | Read          |
+| PUT           | Create      	|  
+| POST          | Update      	|  
+| DELETE        | delete      	|
+
+- Using as Middleware
+	- Express
+	- Body-parser
+	- 
+==Requirements==
+1. Node
+2. MongoDB
+  a. Create a DB
+  b. Select DB 
+  c. Create a collection with name task
+  x. [check] (http://mongodb.github.io/node-mongodb-native/2.2/quick-start/quick-start/)
+
+```javascript
+
+	mongod -> starts service
+	mongo -> conects to db
+
+	show dbs -> show databases
+	use --xxxx -> select database
+	show collections -> shows tables on selected db
+	//Not required
+	db.createUser ({ user: "list",
+	  pwd: "1234",
+	  roles: ["readWrite","dbAdmin"]
+	});
+
+´´´
+
+Download repository
+npm install
+
+*** RUN APP ***
+>? node index.js
+
+
+
+*FYI:* 
+[PUMP my README FILE -> markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+ to style my README file 
--- index.js
+++ index.js
@@ -1,13 +1,107 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+// Create server
+var express = require("express");
+var app = express();
+var port = 5000;
+// DB Handler
+var mongoose = require("mongoose");
 
-// Connection URL
+// Middleware parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define DB connection
+	// pre-required
+	// on terminal or console: 1. Create collection with Mongo
+	// db.createCollection('tasks')
+
+// Use Mongoose connect method to connect to the server
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/mylist")
+
+// Finish DB connection
+
+// Mongose Schema
+var dataSchema = new mongoose.Schema({
+	data1: String,
+	data2: String
+});
+var postData = mongoose.model("postData", dataSchema);
+
+// APP
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+// Endpoint *addName* solving promise
+	// *slash* adddata is what resolves the form in the html doc when hit the button-
+app.post("/adddata", (req, res) => {
+	var myData = new postData(req.body);
+	 myData.save()
+	 	.then(item => {
+	 		res.send("item saved to database");
+	 		console.log("item saved to database");
+	 	})
+	 	.catch(err => {
+	 		res.status(400).send("unable to save to database");
+	 	});
+});
+
+app.listen(port, () => {
+	console.log("Server listening on port " + port);
+});
+
+
+
+/* No Mongoose version*/
+// Local Connection URL
+/*
 var url = 'mongodb://localhost:27017/mylist';
-
-// Use connect method to connect to the server
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
   db.close();
 });
+
+var insertDocument = function(db, callback) {
+   db.collection('tasks').insertOne( {
+      "address" : {
+         "street" : "0 AAAAAA",
+         "zipcode" : "0000000",
+         "building" : "BBBBBB",
+         "coord" : [ -73.9557413, 40.7720266 ]
+      },
+      "borough" : "Buenos Aires",
+      "cuisine" : "Italian",
+      "grades" : [
+         {
+            "date" : new Date("2014-10-01T00:00:00Z"),
+            "grade" : "A",
+            "score" : 11
+         },
+         {
+            "date" : new Date("2014-01-16T00:00:00Z"),
+            "grade" : "B",
+            "score" : 17
+         }
+      ],
+      "name" : "CHANGO",
+      "restaurant_id" : "0000000"
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback();
+  });
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  insertDocument(db, function() {
+      db.close();
+  });
+});
+*/
+console.log("IndexJS Loaded");
